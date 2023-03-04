c614269412c40f2a1cd6bd6ba08159d81dca68a0
Shiya Kohn <shiyaliebling@gmail.com>
2023-03-04 15:58:18 -0500

    Basic store front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import StoreHeader from './components/StoreHeader';
+import Products from './components/Products';
+import StoreFooter from './components/StoreFooter';
+
+function App() {
+  return (<main>
+    <StoreHeader />
+    <Products />
+    <StoreFooter />
+   </main>);
+}
+
+export default App;
