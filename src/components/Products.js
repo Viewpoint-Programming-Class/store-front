de8210c27868a46aaf9ce9dd523f0235ed9231f4
Josh Kohn <skohn@goflowapp.com>
2023-03-05 14:12:57 -0500

    Pass actual params
    Now that we have prepared the Product component to accept product information, we can now go and pass in the props where we create the product component, in the Products component, the one where we list the products.
    
    Now that we are using dynamic props (the text isn't hardcoded in the Product component but passed in from the Products list component), we can use different products for each line, we will get the same beautiful style and buttons, with a different image and text for each line.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,37 @@
 import Product from './Product';
 import './Products.css';
 
+const products = [
+    {
+        name: 'Chocolate Mask',
+        description: 'Handmade Belgian Dark Chocolate Mask Pop',
+        image: '/images/choc-mask.jpg',
+        price: 3.25,
+    },
+    {
+        name: 'Sumchas Purim Mask',
+        description: 'Scratch Art Purim Masks Kit',
+        image: '/images/sumchas-mask.jpg',
+        price: 5.00,
+    },
+]
+
 const Products = () => {
     return <article>
         <h1>Products</h1>
         <ul>
-           <Product />
-           <Product />
-           <Product />
-           <Product />
-           <Product />
+           <Product
+                name={products[0].name}
+                description={products[0].description}
+                image={products[0].image}
+                price={products[0].price}
+           />
+            <Product
+                name={products[1].name}
+                description={products[1].description}
+                image={products[1].image}
+                price={products[1].price}
+            />
         </ul>
     </article>;
 }
